507ce0163b3e025333b89ee856577f277598c915
Kovács <Norbert.Kovacs@kpmg.hu>
2020-03-07 15:50:50 +0100

    Added  'users/verifyUser' route

--- routes/users.js
+++ routes/users.js
@@ -55,8 +55,15 @@ router.route('/login')
     var token = authenticate.getToken({_id: req.user._id})
     res.statusCode = 200;
     res.setHeader('Content-Type','application/json');
-    res.json({success: true, token:token, status: 'Your are successfully logged in'})  
+    res.json({success: true, token:token, status: 'You are successfully logged in'})  
   });
+
+router.route('/verifyUser')
+  .post(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+      res.statusCode = 200;
+      res.setHeader('Content-Type','application/json');
+      res.json({success: true, status: 'You are successfully logged in'});
+  })
 
 router.route('/logout')
   .options(cors.corsWithOptions, (req, res) => { res.sendStatus(200); })
